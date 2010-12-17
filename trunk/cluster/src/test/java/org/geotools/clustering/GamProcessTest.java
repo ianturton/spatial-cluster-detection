8273d0ba54361b5b2cf3c5102590d5be37f39f2d
ijturton <ijturton@c451729b-2f3b-e42b-e5eb-038d269f3e2e>
2010-12-17 16:54:05 +0000

    added a stub test for GamProcess and deleted spare class from mvn setup

---
+++ trunk/cluster/src/test/java/org/geotools/clustering/GamProcessTest.java
@@ -0,0 +1,35 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2004-2010, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.geotools.clustering;
+
+import junit.framework.TestCase;
+
+/**
+ * @author ijt1
+ *
+ */
+public class GamProcessTest extends TestCase {
+    
+        
+        public void testGamProcess() throws Exception {
+            
+            // do some more assets to validate the result of your process
+        }
+
+
+}
